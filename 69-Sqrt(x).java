2b28579fa9e1e4e3524a505e1565259570890bf3
vatsalagarwal610 <vatsal.agarwal610@gmail.com>
2026-08-09 01:35:10 +0530

    Implement mySqrt method for integer square root
    Implemented a method to compute the integer square root of a given number using Java's Math.sqrt function.

---
+++ 69-Sqrt(x).java
@@ -0,0 +1,18 @@
+//Problem No.: 69
+//Problem Name: Sqrt(x)
+//Difficulty: EASY 
+
+
+//SOLUTION:-
+
+class Solution {
+    public int mySqrt(int x) {
+        int s = (int)Math.sqrt(x);
+        return s;
+        
+    }
+}
+
+
+//Run Time: 1ms | 97.81%   
+//Memory: 42.89 MB | 8.53%
