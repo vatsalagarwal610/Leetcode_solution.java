967201c8be502734039f818500bda15718bce67e
vatsalagarwal610 <vatsal.agarwal610@gmail.com>
2026-08-11 18:49:56 +0530

    Add method to reverse words in a string
    Implement a method to reverse the order of words in a given string while maintaining their original order.

---
+++ 151-ReverseWordsIn_a_String.java
@@ -0,0 +1,35 @@
+//Problem No.: 151
+//Problem Name: Reverse Words in a String
+//Difficulty: MEDIUM
+
+
+//SOLUTION:-
+
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        int right = s.length() - 1;
+        while (right >= 0) {
+            while (right >= 0 && s.charAt(right) == ' ') {
+                right--;
+            }
+            if (right < 0) break;
+
+            int left = right;
+            while (left >= 0 && s.charAt(left) != ' ') {
+                left--;
+            }
+            if (result.length() > 0) {
+                result.append(" ");
+            }
+            result.append(s.substring(left + 1, right + 1));
+
+            right = left;
+        }
+        return result.toString();
+    }
+
+
+//Run Time: 4ms | 92.67%   
+//Memory: 44.15 MB | 79.95%
+}
